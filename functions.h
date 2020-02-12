7505df6f42bafa3dc9da19e7b847689dbcba337a
IZacNweTon <mlitaAndHopes@yandex.ru>
2020-02-12 19:54:17 +0300

    modified search functions and added print

--- functions.h
+++ functions.h
@@ -4,9 +4,10 @@
 #include<stdint.h>
 #include<math.h>
 #include<iostream>
+#include<fstream>
+#include<string.h>
 
-
-void Eratosthenes(uint32_t max, primes& cont){
+void Eratosthenes(primes& cont, uint32_t max = 100){
     cont = primes(max);
     bool *arr = new bool[max];
     for(uint32_t i = 2; i < max; i++) arr[i] = true;
@@ -41,5 +42,48 @@ void Modified_search(uint32_t amount, primes& arr){
         current++;
     }
 }
+bool is_palindrome(uint32_t var){
+    int cpy = var;
+    int temp = 0;
+    while( (var / 10) > 0){
+        temp = temp * 10 + (var % 10);
+        var /= 10;
+    }
+    temp = temp * 10 + var % 10;
+    if(temp == cpy) return true;
+    else return false;
+}
+
+
+
+bool print(primes& arr, std::ostream& output = std::cout, std::string type = "none"){
+    if(type == "none"){
+        for(int i = 0; i < arr.size(); i++){
+            output << arr[i] << std::endl;
+        }
+    }else if(type == "superprime"){
+        int i = 0;
+        while(arr[i] <= arr.size()){
+            output << arr[arr[i] - 1] << std::endl;
+            i++;
+        }
+    }else if(type == "palindrome"){
+        for(int i = 0; i < arr.size(); i++){
+            if(is_palindrome(arr[i])) output << arr[i] << std::endl;
+        }
+    } else{
+        std::cout << "unknown sequence. try 'palindrome' or 'superptime' " << std::endl;
+        return false;
+    }
+    std::cout << "successfully written to file" << std::endl;
+    return true;
+}
+
+bool is_sequence(const char* str){
+    if(strcmp(str, "superprime") == 0 || strcmp(str, "palindrome") == 0) return true;
+    else return false;
+}
+
+
 
 #endif // FUNCTIONS_H
